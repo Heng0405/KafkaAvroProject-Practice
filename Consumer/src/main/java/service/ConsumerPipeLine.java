78d9360c97a74731a35d7af53b1afeb6ea4ddec1
heng <fluty1116@gmail.com>
2020-04-28 18:45:47 +0200

    sparksteaming fait la calcule

--- Consumer/src/main/java/service/ConsumerPipeLine.java
+++ Consumer/src/main/java/service/ConsumerPipeLine.java
@@ -1,50 +1,92 @@
 package service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import config.ApplicationProperties;
 import io.confluent.kafka.serializers.KafkaAvroDeserializer;
-import io.confluent.kafka.serializers.KafkaAvroSerializerConfig;
-import org.apache.avro.generic.GenericRecord;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
+import model.TweetObject;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.serializer.KryoSerializer;
 import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.spark.streaming.kafka010.*;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.io.Serializable;
 import java.util.*;
 
-public class ConsumerPipeLine {
+public class ConsumerPipeLine implements Serializable {
     private static final org.slf4j.Logger logger = LoggerFactory.getLogger(ConsumerPipeLine.class);
+    ObjectMapper objectMapper = new ObjectMapper();
+
+
+
 
     public ConsumerPipeLine() {
     }
 
+
+
     public void run() throws IOException {
         logger.info("----------------------Start Stream-----------------------------");
 
-/*        SparkConf conf = new SparkConf().setMaster("local[2]").setAppName("ConsumerPipeLine");
-        JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(1));*/
+        SparkConf conf = new SparkConf().setMaster("local[2]").setAppName("ConsumerPipeLine");
+        conf.set("spark.serializer", KryoSerializer.class.getName());
+        JavaStreamingContext streamingContext = new JavaStreamingContext(conf, Durations.seconds(1));
+
+
+        Map<String, Object> kafkaParams = new HashMap<String, Object>();
+        Properties properties = ApplicationProperties.getInstance();
+        kafkaParams.put("bootstrap.servers", properties.getProperty("kafka.bootstrap.servers"));
+        kafkaParams.put("key.deserializer", StringDeserializer.class.getName());
+        kafkaParams.put("value.deserializer",KafkaAvroDeserializer.class.getName());
+        kafkaParams.put("group.id", properties.getProperty("group.id"));
+        kafkaParams.put("auto.offset.reset", properties.getProperty("auto.offset.reset"));
+        kafkaParams.put("schema.registry.url",properties.getProperty("kafka.schema.registry.url"));
+        kafkaParams.put("enable.auto.commit", false);
+
+        Collection<String> topics = Arrays.asList("TwitterAvro");
+
+
+        JavaInputDStream<ConsumerRecord<String, Object>> stream =
+                KafkaUtils.createDirectStream(
+                        streamingContext,
+                        LocationStrategies.PreferConsistent(),
+                        ConsumerStrategies.<String,Object>Subscribe(topics,kafkaParams)
+                );
+        JavaDStream<String> lines = stream.map(stringStringConsumerRecord -> stringStringConsumerRecord.value().toString());
+
+        List<TweetObject> tweetObjects = new ArrayList<>();
+
+
+        lines.foreachRDD(rdd ->{
+            JavaRDD<TweetObject> rddTwitter = rdd.map(mappedRdd -> objectMapper.readValue(mappedRdd.getBytes(),TweetObject.class)).cache();
+        /*    JavaPairRDD<Integer,String> retweets = rddTwitter.mapToPair(retwittesCount -> new Tuple2<>(retwittesCount.getRetweetCount(),retwittesCount.tweetText));
+            retweets.sortByKey(false).take(10);  //the most retwitted twittes.
+            retweets.foreach(x-> System.out.println("test---------------------------"+x));*/
+
+            rddTwitter.foreach(x -> System.out.println("test--------"+x.displayName+"------------"+x.statusId+"---------------"+x.getTweetText()));
 
 
 
-        //Map<String, Object> kafkaParams = new HashMap<String, Object>();
-        Properties kafkaParams = ApplicationProperties.getInstance();
-        kafkaParams.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaParams.getProperty("kafka.bootstrap.servers"));
-        kafkaParams.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        kafkaParams.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class.getName());
-        kafkaParams.put(ConsumerConfig.GROUP_ID_CONFIG, kafkaParams.getProperty("group.id"));
-        kafkaParams.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, kafkaParams.getProperty("auto.offset.reset"));
-        kafkaParams.put(KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, kafkaParams.getProperty("kafka.schema.registry.url"));
-        kafkaParams.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
 
+        });
 
+        // Start the computation
+        streamingContext.start();
+        try {
+            streamingContext.awaitTermination();
+            logger.info("Stop the job");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-        KafkaConsumer<String, GenericRecord> consumer = new KafkaConsumer<String, GenericRecord>(kafkaParams);
+/*        KafkaConsumer<String, GenericRecord> consumer = new KafkaConsumer<String, GenericRecord>(kafkaParams);
         consumer.subscribe(Arrays.asList("TwitterAvro"));
         try {
             while (true) {
@@ -58,7 +100,7 @@ public class ConsumerPipeLine {
             }
         } finally {
             consumer.close();
-        }
+        }*/
 
 
 
--- TwitterModel/src/main/java/model/TweetObject.java
+++ TwitterModel/src/main/java/model/TweetObject.java
@@ -1,8 +1,9 @@
 package model;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class TweetObject {
+public class TweetObject implements Serializable {
 
     public TweetObject(){};
 
